1ff5e9c94a82d5fd9b41df729782bef928e9e1eb
bryanpiguave <bpiguave@espol.edu.ec>
2021-11-25 09:07:14 -0500

    Adding first implementation in python

---
+++ main file.py
@@ -0,0 +1,92 @@
+#main file
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+ON = 255 
+OFF= 0
+
+values = [ON,OFF]
+
+def randomGrid(N):
+    return np.random.choice(values,size=N*N,p=[0.2,0.8]).reshape(N,N)
+
+def update(frameNum, img, grid, N):
+ 
+    # copy grid since we require 8 neighbors
+    # for calculation and we go line by line
+    newGrid = grid.copy()
+    for i in range(N):
+        for j in range(N):
+ 
+            # compute 8-neighbor sum
+            # using toroidal boundary conditions - x and y wrap around
+            # so that the simulaton takes place on a toroidal surface.
+            total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] +
+                         grid[(i-1)%N, j] + grid[(i+1)%N, j] +
+                         grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] +
+                         grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
+ 
+            # apply Conway's rules
+            if grid[i, j]  == ON:
+                if (total < 2) or (total > 3):
+                    newGrid[i, j] = OFF
+            else:
+                if total == 3:
+                    newGrid[i, j] = ON
+ 
+    # update data
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+    return img,
+
+def main():
+ 
+    # Command line args are in sys.argv[1], sys.argv[2] ..
+    # sys.argv[0] is the script name itself and can be ignored
+    # parse arguments
+    parser = argparse.ArgumentParser(description="Runs Conway's Game of Life simulation.")
+ 
+    # add arguments
+    parser.add_argument('--grid-size', dest='N', required=False)
+    parser.add_argument('--mov-file', dest='movfile', required=False)
+    parser.add_argument('--interval', dest='interval', required=False)
+    args = parser.parse_args()
+     
+    # set grid size
+    N = 120
+    if args.N and int(args.N) > 8:
+        N = int(args.N)
+         
+    # set animation update interval
+    updateInterval = 50
+    if args.interval:
+        updateInterval = int(args.interval)
+ 
+    # declare grid
+    grid = np.array([])
+ 
+    grid = randomGrid(N)
+ 
+    # set up animation
+    fig, ax = plt.subplots()
+    ax.set_title("Conway's Game of Life")
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    img = ax.imshow(grid, interpolation='nearest')
+    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
+                                  frames = 10,
+                                  interval=updateInterval,
+                                  save_count=50)
+ 
+    # # of frames?
+    # set output file
+    if args.movfile:
+        ani.save(args.movfile, fps=30, extra_args=['-vcodec', 'libx264'])
+ 
+    plt.show()
+ 
+# call main
+if __name__ == '__main__':
+    main()
